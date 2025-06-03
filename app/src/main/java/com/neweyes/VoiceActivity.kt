eed413630db0ac624479f4b6ed96e7c02dd2dee9
siok <eladiohv2@gmail.com>
2025-06-03 10:14:16 -0500

    Add voice recognition

--- app/src/main/java/com/neweyes/VoiceActivity.kt
+++ app/src/main/java/com/neweyes/VoiceActivity.kt
@@ -1,6 +1,7 @@
 package com.neweyes
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
@@ -11,6 +12,11 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.neweyes.databinding.ActivityVoiceBinding
+import android.content.Intent
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import java.util.*
 
 class VoiceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVoiceBinding
@@ -28,7 +34,33 @@ class VoiceActivity : AppCompatActivity() {
         } else {
             requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
+
+        binding.btnMic.setOnClickListener {
+            if (!SpeechRecognizer.isRecognitionAvailable(this)) {
+                Toast.makeText(this, "Reconocimiento de voz no disponible", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+                putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+                putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+                putExtra(RecognizerIntent.EXTRA_PROMPT, "Di tu destino...")
+            }
+
+            voiceRecognizerLauncher.launch(intent)
+        }
+
     }
+
+    @SuppressLint("SetTextI18n")
+    private val voiceRecognizerLauncher = registerForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            val results = result.data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val spokenText = results?.get(0) ?: ""
+            binding.tvSpeechResult.text = "Destino: $spokenText"
+        }
+    }
+
 
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
